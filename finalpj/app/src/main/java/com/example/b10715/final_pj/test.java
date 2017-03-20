c24c495b5e7170ec933857ca4d3690aeb0d65bac
mina693 <mina693@hanmail.net>
2017-03-20 18:24:39 +0900

    minachange
    test

--- finalpj/app/src/main/java/com/example/b10715/final_pj/test.java
+++ finalpj/app/src/main/java/com/example/b10715/final_pj/test.java
@@ -5,4 +5,10 @@ package com.example.b10715.final_pj;
  */
 
 public class test {
+    final static int RAW=0;
+    final static int SDCARD=1;
+    final static int WEB=2;
+    final static int AUDIO=3;
+    final static int VIDEO=4;
+    final static int IMAGE=5;
 }
